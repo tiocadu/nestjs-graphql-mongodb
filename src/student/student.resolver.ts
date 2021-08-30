a14b5041d1f1c3dbee35c35af4e247e79925d218
Carlos Eduardo <edugaspar@hotmail.com>
2021-08-29 21:52:00 -0300

    adds query students

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentService } from './student.service';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
@@ -6,6 +6,11 @@ import { CreateStudentInput } from './create-student.input';
 @Resolver((of) => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation((returns) => StudentType)
   createStudent(
